41c1470c87043355a58ef3e673bbf5ed97f615da
Samet Bilir <smtblr33@hotmail.com>
2026-08-11 13:13:20 +0300

    fix to About app

--- HolmiumOS/GUI/Apps/About.cs
+++ HolmiumOS/GUI/Apps/About.cs
@@ -1,73 +1,91 @@
 ﻿using System;
 using HolmiumOS.GUI.Controls;
 using Cosmos.Core;
+using Cosmos.Core.Multiboot;
 using Sys = Cosmos.System;
+using System.Drawing;
 
 namespace HolmiumOS.GUI.Apps
 {
     public class About : AppBase
     {
-        private Label osInfoLabel;
-        private Label cpuInfoLabel;
-        private Label vmInfoLabel;
-        private Label ramTextLabel;
-
         public About() : base("Hakkinda")
         {
         }
 
         public override void Load()
         {
+            if (Window != null)
+            {
+                Window.Title = "Sistem Hakkinda";
+            }
+
+            Label headerLabel = new Label("HolmiumOS GUI Ortami", 20, 15);
+
+            string cpuName = "x86/x64 Islemci";
+            string vmType = "Bilinmiyor";
+            string ramInfo = "RAM: Bilgi Alinamadi";
+            string storageInfo = "Depolama: Bilgi Alinamadi";
+            int storagePercent = 0;
+
             try
             {
-                if (this.Window != null)
+                string brand = CPU.GetCPUBrandString();
+                if (!string.IsNullOrEmpty(brand)) cpuName = brand;
+
+                vmType = Sys.VMTools.IsVMWare ? "VMWare" :
+                         Sys.VMTools.IsVirtualBox ? "VirtualBox" :
+                         Sys.VMTools.IsQEMU ? "QEMU" : "Fiziksel";
+
+                ulong totalRamMB = Multiboot2.GetMemUpper() / 1024;
+                ramInfo = $"RAM: {totalRamMB} MB Toplam";
+
+                var fs = Kernel.fs;
+                string driveId = "0:";
+                ulong totalBytes = (ulong)fs.GetTotalSize(driveId);
+                ulong freeBytes = (ulong)fs.GetAvailableFreeSpace(driveId);
+                ulong usedBytes = totalBytes - freeBytes;
+
+                double totalGB = totalBytes / 1024.0 / 1024.0 / 1024.0;
+
+                if (totalBytes > 0)
                 {
-                    this.Window.Title = "Sistem Hakkinda";
+                    storagePercent = (int)((usedBytes * 100) / totalBytes);
                 }
 
-                Label headerLabel = new Label("HolmiumOS GUI Ortami", 20, 15);
-
-                osInfoLabel = new Label("Surum: " + Kernel.OSVERSION, 20, 35);
-
-                string cpuName = "x86/x64 Islemci";
-                try
+                if (usedBytes >= 1024L * 1024L * 1024L)
                 {
-                    cpuName = CPU.GetCPUBrandString();
-                    if (string.IsNullOrEmpty(cpuName)) cpuName = "x86/x64 Islemci";
+                    double usedGB = usedBytes / 1024.0 / 1024.0 / 1024.0;
+                    storageInfo = $"Depolama: {usedGB:F2} GB / {totalGB:F2} GB";
                 }
-                catch { }
-                cpuInfoLabel = new Label("CPU: " + cpuName, 20, 60);
-
-                string vmType = "Bilinmiyor";
-                try
+                else
                 {
-                    vmType = Sys.VMTools.IsVMWare ? "VMWare" :
-                             Sys.VMTools.IsVirtualBox ? "VirtualBox" :
-                             Sys.VMTools.IsQEMU ? "QEMU" : "Fiziksel";
-                }
-                catch { }
-                vmInfoLabel = new Label("Ortam: " + vmType, 20, 80);
-
-                string ramInfo = "RAM: Bilgi Alinamadi";
-                try
-                {
-                    uint totalRamMB = CPU.GetAmountOfRAM();
-                    ramInfo = "Toplam RAM: " + totalRamMB.ToString() + " MB";
-                }
-                catch { }
-                ramTextLabel = new Label(ramInfo, 20, 110);
-
-                if (this.Window != null)
-                {
-                    this.Window.AddControl(headerLabel);
-                    this.Window.AddControl(osInfoLabel);
-                    this.Window.AddControl(cpuInfoLabel);
-                    this.Window.AddControl(vmInfoLabel);
-                    this.Window.AddControl(ramTextLabel);
+                    double usedMB = usedBytes / 1024.0 / 1024.0;
+                    storageInfo = $"Depolama: {usedMB:F2} MB / {totalGB:F2} GB";
                 }
             }
             catch
             {
+            }
+
+            Label cpuInfoLabel = new Label("CPU: " + cpuName, 20, 35);
+            Label vmInfoLabel = new Label("Ortam: " + vmType, 20, 55);
+            Label ramTextLabel = new Label(ramInfo, 20, 75);
+            Label storageTextLabel = new Label(storageInfo, 20, 95);
+
+            ProgressBar storageBar = new ProgressBar(20, 120, 240, 15);
+            storageBar.Value = storagePercent;
+            storageBar.BarColor = Color.DodgerBlue;
+            storageBar.BackgroundColor = Color.LightGray;
+
+            if (Window != null)
+            {
+                Window.AddControl(headerLabel);
+                Window.AddControl(cpuInfoLabel);
+                Window.AddControl(vmInfoLabel);
+                Window.AddControl(ramTextLabel);
+                Window.AddControl(storageTextLabel);
+                Window.AddControl(storageBar);
             }
         }
 
